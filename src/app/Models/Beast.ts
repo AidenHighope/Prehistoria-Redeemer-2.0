f9e7bca83c35b2e865fd637abe7502fc2db787c0
Aiden <afnopere@gmail.com>
2024-01-08 15:30:49 +0100

    starting working on lineage checker and breeding roller

--- src/app/Models/Beast.ts
+++ src/app/Models/Beast.ts
@@ -1,7 +1,68 @@
 import { tack } from './Game-item';
 
 export interface beast {
-  species: string;
+  name: string;
+  nickname?: string;
   ID: string;
+  owner: string;
   equipped: tack[];
+  geno: Geno;
+  lineage: Lineage[];
 }
+
+export interface Geno {
+  baseCoat: string;
+  markings: Markings[];
+  mane: string[];
+  tail: string;
+  mutation: string;
+}
+
+export interface Markings {
+  name: string;
+  rarity: string;
+}
+
+export interface Lineage {
+  sire: string;
+  dam: string;
+  ss: string;
+  sss: string;
+  ssd: string;
+  sd: string;
+  sds: string;
+  sdd: string;
+  ds: string;
+  dss: string;
+  dsd: string;
+  dd: string;
+  dds: string;
+  ddd: string;
+}
+/* 
+note : those letters means the following :
+ss  : sire of the sire (father side grandpa)
+sss : sire of the "sire sire" (father side great grandpa)
+sd  : dam of the sire (father side grandma)
+ssd : dam of the "sire sire" (father side great grandma)
+
+and same idea for the Dam letters.
+
+those letters are used to check inbreeding.
+
+in game example of a lineage :
+----------------------------------SSS: Wild
+---------------SS: Mahamba D139
+----------------------------------SSD: Wild
+Sire: North WD715
+----------------------------------SDS: Wild
+---------------SD: Rikili D282
+----------------------------------SDD: Wild
+----------------------------------DSS: Storm D284
+---------------DS: Loki D368
+----------------------------------DSD: Frost D09
+Dam: Aurelia D647
+----------------------------------DDS: Altair D47
+---------------DD: Hayley D246
+----------------------------------DDD: Naluaa D58
+*/
